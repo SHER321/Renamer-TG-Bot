a02bd02fa0c0271043a0ab5258263cfb5a5ba23b
SHER321 <66766275+SHER321@users.noreply.github.com>
2020-06-17 13:19:00 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -2,17 +2,18 @@ class Translation(object):
     START_TEXT = """Hello,
 This is a Telegram Rename Bot!
 <b>Please send me any Telegram file and reply to that file to /rename New Name.mkv</b>
+<a href="https://tx.me/NGYNY">😎 N I K H I L 😎</a>
 /help for more details.."""
     RENAME_403_ERR = "Sorry. You are not permitted to rename this file."
     ABS_TEXT = " Please don't be selfish."
-    UPGRADE_TEXT = "<b>👉 join our Updates Channel @NRBotsZ. </b>  /help for Details"
+    UPGRADE_TEXT = "<b>👉 join our Updates Channel @ALL_MOVIES_LIABRARY. </b>  /help for Details"
     DOWNLOAD_START = "Trying to Download"
     UPLOAD_START = "Trying to Upload"
     RCHD_TG_API_LIMIT = "Downloaded in {} seconds.\nDetected File Size: {}\nSorry. But, I cannot upload files greater than 1.5GB due to Telegram API limitations."
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me🤓.** \n Join:- @NRBotsZ"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank you for Using Me🤓.** \n Join:- @ALL_MOVIES_LIABRARY"
     AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS = "Downloaded in {} seconds.\nUploaded in {} seconds."
     NOT_AUTH_USER_TEXT = "Please /upgrade your subscription."
-    NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/NRBotsZ>@NRBotsZ</a>"
+    NOT_AUTH_USER_TEXT_FILE_SIZE = "Detected File Size: {}. Free Users can only upload: {}\nPlease /upgrade your subscription.\nIf you think this is a bug, please contact <a href='https://telegram.dog/ALL_MOVIES_LIABRARY>ᏗᏝᏝ ᎷᎧᏉᎥᏋᏕ ᏝᎥᏰᏒᏗᏒᎩ</a>"
     SAVED_CUSTOM_THUMB_NAIL = "Custom File thumbnail saved. This image will be used in the File."
     DEL_ETED_CUSTOM_THUMB_NAIL = "✅ Custom thumbnail cleared succesfully."
     FF_MPEG_DEL_ETED_CUSTOM_MEDIA = "✅ Media cleared succesfully."
@@ -20,15 +21,22 @@ This is a Telegram Rename Bot!
     CUSTOM_CAPTION_UL_FILE = " "
     NO_CUSTOM_THUMB_NAIL_FOUND = "No Custom ThumbNail found."
     USER_ADDED_TO_DB = "User <a href='tg://user?id={}'>{}</a> added to {} till {}."
-    HELP_USER = """Hai am Rename bot..
-    
-1. Send me any Telegram File.
-2. Reply to that message to /rename new name.extension.
-   
+    HELP_USER = """<b>How To Use Me</b>
 
---------
-Support Group : @NRBotsZ
-"""
+    1. Send me any Telegram File(Some Files Gets Error)
+
+    2. Reply to that message to /rename NEW NAME.EXTENSION
+
+    FEATURES
+
+    👉Rename Telegram File With Custom Thumbnail.
+
+    👉Convert Streamable File(Video) To File.
+
+    👉FOR Downloading Files At Highspeed Join @ALL_MOVIES_LIABRARY"""
+
+
+
     REPLY_TO_DOC_FOR_RENAME_FILE = "Reply to a Telegram media to `/rename New Name.extension` with custom thumbnail support.."
     ABUSIVE_USERS = "You are not allowed to use this bot. If you think this is a mistake, please check /me to remove this restriction."
     FREE_USER_LIMIT_Q_SZE = """Cannot Process.
@@ -37,5 +45,5 @@ Free users only 1 request per 30 minutes.
     IFLONG_FILE_NAME = """File Name limit allowed by Telegram is {alimit} characters.
 The given file name has {num} characters.
 <b>Essays Not allowed in Telegram file name!</b>
-©️ <code>@NRBotsZ</code>
+©️ <code>@ALL_MOVIES_LIABRARY</code>
 Please short your file name and try again!"""
